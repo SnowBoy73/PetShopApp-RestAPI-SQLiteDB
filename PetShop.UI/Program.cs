f10864fe2cada741807e337cbd707c15f34409b4
SnowBoy73 <56825266+SnowBoy73@users.noreply.github.com>
2020-09-12 13:54:01 +0200

    init

---
+++ PetShop.UI/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using PetShop.Core.ApplicationService;
+using PetShop.Core.ApplicationService.Impl;
+using PetShop.Core.DomainService;
+using PetShop.Infrastructure.Data.Repositories;
+
+namespace PetShop.RestAPI
+{
+    class Program
+    {
+        #region Main
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();                // start old
+            serviceCollection.AddScoped<IPetRepository, PetRepository>();
+            serviceCollection.AddScoped<IPetService, PetService>();
+            serviceCollection.AddScoped<IPrinter, Printer>();
+            serviceCollection.AddScoped<IFakeDB, FakeDB>();  // NEW
+
+            // Build provider
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var printer = serviceProvider.GetRequiredService<IPrinter>();
+            var fakeDB = serviceProvider.GetRequiredService<IFakeDB>();
+
+            //Initialise and Start
+            fakeDB.InitData();
+            printer.StartUI();                                              // end old
+
+            CreateHostBuilder(args).Build().Run();
+        }
+        #endregion
+
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
